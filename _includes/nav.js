b3491d3d685b8e433d2027b9f925d4d64ec3feaa
Martí <marnauortega@gmail.com>
2025-11-23 20:22:04 +0100

    feat(clean up)

---
+++ _includes/nav.js
@@ -0,0 +1,17 @@
+class MyNav extends HTMLElement {
+  connectedCallback() {
+    const current = this.getAttribute("current");
+    this.innerHTML = `
+        <header>
+		    <h1 class="leonie"><a href="/">LEONIE WIMMER</a></h1>
+			<nav>
+				<a class="nav-link ${!current || current === "singlepage" ? "active" : ""}" href="index.html"> work </a>
+				<a class="nav-link ${current === "about-me" ? "active" : ""}" href="about-me.html"> about me </a>
+				<a class="nav-link ${current === "contact" ? "active" : ""}" href="contact.html"> contact </a>
+			</nav>
+        </header>
+        `;
+  }
+}
+
+customElements.define("my-nav", MyNav);
